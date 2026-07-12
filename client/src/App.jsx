849a3884d0864e3e48fc08c95fd93ce06255ee98
Harshavardhana B <harshabhaskar456@gmail.com>
2026-07-13 01:39:50 +0530

    Update production build with live Render URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Retail-pos-system">
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
